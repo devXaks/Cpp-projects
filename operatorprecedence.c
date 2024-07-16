3459576681b4427a6b27b1ddb7702cd7a2a59334
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-07-16 20:30:07 +0530

    added files

---
+++ operatorprecedence.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+
+printf("%f",3.0/4-2);
+} 
---
+++ variable_dec.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float a = 0.10;
+    int b = 2 ;
+    float c;
+    c = a+b;
+    printf("%d",c);
+}

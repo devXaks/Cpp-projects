e03e38fb0669b1bb3fbe072517f5b3324f78eb12
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-07-25 17:14:17 +0530

    updated files

--- pattern_new_4.cpp
+++ pattern_new_4.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main(){
     int x;
     cin>>x;
-    int cnt = 1;
-    for(int i =1 ;i<=x;i++ ){
+    int cnt;
+    for(int i=1;i<=x;i++){
+        cnt = i;
         for(int j=1;j<=i;j++){
             cout<<cnt<<" ";
             cnt++;
         }
-        cout<<endl;
-
+        cout<<"\n";
     }
     return 0;
 }

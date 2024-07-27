10b520d5cf430a870ff9b950dc5951db4be66d2d
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-07-27 11:34:16 +0530

    updated files

--- old/tempCodeRunnerFile.cpp
+++ old/tempCodeRunnerFile.cpp
@@ -1,2 +1,30 @@
+#include <iostream>
+
+using namespace std; 
 
 int main(){
+    int row = 1;
+    int clmn =1;
+    int num;
+    int chr;
+    cin>>num;
+
+    while(row<=num){
+        chr = 'A';
+        clmn = 1;
+
+        while(clmn<= num){
+            cout<<char(chr)<<" ";
+            clmn++;
+
+
+            chr++;
+            
+
+        }
+        row++;
+        cout<<"\n";
+
+    }
+    return 0;
+}
---
+++ pattern_new_10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+    int num;
+    cin>>num;
+    int ctr;
+    ctr = num;
+    for (int i = ctr; i = num; i++){
+        for(int j =1; j<= i;j++){
+            cout<<
+        }
+    }
+    return 0;
+}

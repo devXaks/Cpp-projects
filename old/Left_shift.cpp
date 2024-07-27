8bfacde862d8f9e13bc8aa510e16721af15f7776
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-07-27 19:57:13 +0530

    added files

---
+++ old/Left_shift.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+    int a = 10;
+    cout<<(15<<2);
+    return 0;
+}
---
+++ old/Right_shift.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+    // int a = 5;
+    // cout<<(a>>2);
+    // return 0;
+    for(int a =0,b=1;a>=0&&b>=1;a--,b--){
+
+        cout<<a<<b;
+    }
+}

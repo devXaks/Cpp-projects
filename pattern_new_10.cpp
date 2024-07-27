b21c4715c21f2f8665cbf8c7cb12a75ff15da74b
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-07-27 20:01:01 +0530

    added files:

--- pattern_new_10.cpp
+++ pattern_new_10.cpp
@@ -8,8 +8,15 @@ int main(){
     int ctr;
     ctr = num;
     for (int i = ctr; i = num; i++){
-        for(int j =1; j<= i;j++){
-            cout<<
+        for(int j =ctr; j<= num;j++){
+            cout<<j<<" ";
+            
+        }
+        ctr--;
+        cout<<"\n";
+
+        if(ctr == 0){
+            break;
         }
     }
     return 0;

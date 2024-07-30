1528e977e2e0969b0f1e9c1b24bef72756b3c7fd
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-07-30 08:33:27 +0530

    updated files

---
+++ arraysintro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+    
+//     int arr[5] ;
+// for(int i = 0;i<=4;i++){
+//     arr[i] = i;
+// }    
+// cout<<arr[10];
+//     return 0;
+
+
+
+
+int arr[3] = {5,6,7};
+cout<<arr[3];
+}
--- old/tempCodeRunnerFile.cpp
+++ old/tempCodeRunnerFile.cpp
@@ -3,28 +3,15 @@
 using namespace std; 
 
 int main(){
-    int row = 1;
-    int clmn =1;
-    int num;
-    int chr;
-    cin>>num;
+    int x;
+    cin>>x;
+    if(x&1==1){
+        cout<<"odd";
 
-    while(row<=num){
-        chr = 'A';
-        clmn = 1;
+    }
 
-        while(clmn<= num){
-            cout<<char(chr)<<" ";
-            clmn++;
-
-
-            chr++;
-            
-
-        }
-        row++;
-        cout<<"\n";
-
+    else{
+        cout<<"odd";
     }
     return 0;
 }

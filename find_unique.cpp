b14fb2664168c7f7dfe97f809e24ceb8053defea
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-08-04 21:42:33 +0530

    updated files

--- find_unique.cpp
+++ find_unique.cpp
@@ -3,14 +3,16 @@
 using namespace std; 
 
 int main(){
-    int arr[7] = {1,3,2,2,3,4,1};
+    // int arr[7] = {1,3,2,2,3,4,1};
     // int size = 7;
     // int ans = 0;
     // for(int i = 0;i<size;i++){
     //     ans = ans^arr[i];
     // }
     // cout<<"The ans is"<<ans;
-    cout<<arr.size();
+    // cout<<arr.size();
+
+    cout<<(8888);
     return 0;
 
 }

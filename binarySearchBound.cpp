e4c09f53b1b53376ba22b5ece50e2542251f38b3
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-08-12 22:35:32 +0530

    updated files

--- binarySearchBound.cpp
+++ binarySearchBound.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 
 using namespace std;
-int searchFirst(int arr, int size, int target)
+int searchFirst(int arr[], int size, int target)
 {
     int start = 0;
     int end = size - 1;
     int mid = start + ((end - start) / 2);
     int firstoccur = -1;
 
-    while (arr[mid] != key)
+    while (start!=end)
     {
-        int mid = start + ((end - start) / 2);
 
-        if (start == end)
+        if (arr[mid] == target )
         {
-            return firstoccur;
+            firstoccur = mid;
+            mid -=1;
 
         }
-        else if (arr[mid] > key)
+        else if (arr[mid] > target)
         {
             end = mid - 1;
         }
-        else if (arr[mid] < key)
+        else if (arr[mid] < target)
         {
             start = mid + 1;
         }
-    }
+                int mid = start + ((end - start) / 2);
 
-    if(arr[mid] = key)
+    }
+    return firstoccur;
 }
 
 int main()
 {
+    int arr[5] ={0,1,2,2,3};
+    int ans = searchFirst(arr,5,2);
+    cout<<ans;
 
     return 0;
 }

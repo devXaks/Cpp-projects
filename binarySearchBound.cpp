9f24f3c8ecf8ae71dc4d74e090dcc916bf2dfe2a
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-08-12 22:35:32 +0530

    updated files

--- binarySearchBound.cpp
+++ binarySearchBound.cpp
@@ -1,37 +1,72 @@
 #include <iostream>
 
 using namespace std;
-int searchFirst(int arr, int size, int target)
+int searchFirst(int arr[], int size, int target)
 {
     int start = 0;
     int end = size - 1;
     int mid = start + ((end - start) / 2);
     int firstoccur = -1;
 
-    while (arr[mid] != key)
+    while (start<=end)
     {
-        int mid = start + ((end - start) / 2);
 
-        if (start == end)
+        if (arr[mid] == target )
         {
-            return firstoccur;
+            firstoccur = mid;
+            end = mid-1;
 
         }
-        else if (arr[mid] > key)
+        else if (arr[mid] > target)
         {
             end = mid - 1;
         }
-        else if (arr[mid] < key)
+        else if (arr[mid] < target)
         {
             start = mid + 1;
         }
+        mid = start + ((end - start) / 2);
+
+    }
+    return firstoccur;
+}
+
+int lastSearch(int arr[],int size,int target){
+    int start = 0;
+    int end = size -1;
+    int mid = start + (end-start)/2;
+    int lastoccur = -1;
+
+    while(start<=end){
+        if(arr[mid]  == target){
+            lastoccur = mid;
+            start = mid+1;
+        }
+        else if(arr[mid] > target){
+            end = mid -1;
+        }
+        else if(arr[mid]< target){
+            start = mid +1; 
+        }
+        mid = start + (end-start)/2;
+
+
     }
 
-    if(arr[mid] = key)
+    return lastoccur;
+
+
+
+
 }
 
 int main()
 {
+    int arr[7] ={3,7,15,20,100,100,230};
+    int ans = searchFirst(arr,7,100);
+    int ans1 = lastSearch(arr,7,100);
+    cout<<ans<<endl;
+    cout<<ans1;
 
     return 0;
 }

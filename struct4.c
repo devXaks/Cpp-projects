12b38f17d1e01fd3d9d79437be99053d69891778
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-08-17 08:04:25 +0530

    updated files

---
+++ struct4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+
+
+int main(){
+    struct Person {
+        char name[40];
+        int salary;
+        int age;
+    };
+
+    struct Person person1;
+    person1.age = 40;
+    strcpy(person1.name,"Rajesh");
+    person1.salary = 80000;
+
+    printf("%s",person1.name);    
+    return 0;
+}

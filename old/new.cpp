55e3effa67aa12e374d14aaa256f35e7eed7d26e
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-08-20 21:59:20 +0530

    udpated files

--- old/new.cpp
+++ old/new.cpp
@@ -2,7 +2,7 @@
 
 using namespace std; 
 
-int main(){
+int main(){     
     cout<<INT16_MIN;
     return 0;
 }
---
+++ test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+typedef float decimal;
+
+int main(){
+    decimal krishna = 19.232;
+    printf("%f",krishna);
+    
+    return 0;
+}

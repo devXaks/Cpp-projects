dca55c06e7f7bc3e6abfd8ced051fb5d196b82eb
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-10-24 08:21:58 +0530

    updated files

---
+++ C++stl/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+
+using namespace std; 
+
+int main(){
+    array<int,5> a = {1,2,3,4,5};
+    cout<<a[0]<<" ";
+    cout<<a.at(2)<<" ";
+    cout<<a.empty()<<" ";
+    cout<<a.front()<<" ";
+    cout<<a.back()<<" ";2
+
+
+    
+    return 0;
+}
---
+++ C++stl/2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std; 
+
+int main(){
+    vector <int> v;    
+    cout<<v.capacity(); 
+
+    //Not quite. v.size gives you the number of elements currently in the vector, while v.capacity tells you how much space has been allocated for the vector in memory. This means capacity is typically larger than or equal to size. Think of it as the number of people at a party vs. how much room you have to host them.
+    v.push_back(1);
+    cout<<endl<<"Capaity is --> "<<v.capacity(); 
+    v.push_back(2);
+    cout<<endl<<"Capaity is --> "<<v.capacity(); 
+    v.push_back(3);
+    cout<<endl<<"Capaity is --> "<<v.capacity(); 
+    cout<<endl<<"Size is --> "<<v.size(); 
+
+
+
+    return 0;
+
+}

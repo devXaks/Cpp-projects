ca0993afa6e8e01f86077898e0d4df3bf3d71741
devXaks <mailtoakshattripathi@gmail.com>
2024-12-12 13:44:40 +0530

    updated files

---
+++ C++stl/parenthesis_matching.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+
+struct parentesis{
+    char* ptr;
+    int top;
+    int size;
+};
+
+void push_s0(parentesis *s1,char s){
+    s1->top++;
+    s1->ptr[s1->top] = s;
+
+}
+
+void pop_s0(parentesis *s1,char s){
+    if(s1->top == -1){
+        cout<<"Invalid Parenthesis Matching paramenters";
+        exit(0);
+    }
+
+    else{
+        if(s < 50){
+            if(s1->ptr[s1->top] == s-1)
+            {s1->top--;}
+            else{
+                cout<<"Parenthesis syntax invalid";
+                exit(0);
+            }
+
+        }
+        else {
+            if(s1->ptr[s1->top] == s-2)
+            {s1->top--;}
+            else{
+                cout<<"Parenthesis syntax invalid";
+                exit(0);
+            }
+            
+        }
+    }
+}
+
+
+int main(){
+    struct parentesis s1;
+
+    string s0 = "[4+}2-(3-2){]";
+    // cin
+    for(int i = 0;s0[i] !=0 ;i++){
+        if(s0[i] == '('||s0[i] == '{'||s0[i] == '[' ){
+        push_s0(&s1,s0[i]);
+        }
+
+        else if(s0[i] == ')'||s0[i] == ']'||s0[i] == '}' ){
+        pop_s0(&s1,s0[i]);
+        }
+
+    
+    }
+
+    return 0;
+}
--- C++stl/stack_using_linkedlist.cpp
+++ C++stl/stack_using_linkedlist.cpp
@@ -3,49 +3,66 @@ using namespace std;
 
 struct stkll {
     int data;
-    int* ptr;
+    stkll* ptr;  // Points to the next element in the stack
 };
 
-void push_stk(int * ptr){
-    ptr = (int*)malloc(sizeof(stkll));
+void push_stk(stkll** top) {
+    int num;
+    cout << "Enter value to push: ";
+    cin >> num;
+
+    // Allocate new node
+    stkll* t = new stkll;
+    t->data = num;
+    t->ptr = *top; // Link to the previous top
+    *top = t;  // Update the top of the stack
 }
 
+void pop_stk(stkll** top) {
+    if (*top == nullptr) {
+        cout << "Stack is empty." << endl;
+        return;
+    }
+    stkll* temp = *top;
+    *top = temp->ptr;  // Move the top to the next element
+    delete temp;  // Deallocate the old top
+}
 
+void prntstk(stkll* top) {
+    if (top == nullptr) {
+        cout << "Stack is empty." << endl;
+        return;
+    }
+    cout << "Stack contents: ";
+    stkll* current = top;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->ptr;  // Move to the next node
+    }
+    cout << endl;
+}
 
-int main(){
-
-    int * ptr = nullptr;
+int main() {
+    stkll* top = nullptr;  // Stack is initially empty
     int choice;
 
-    cout<<ptr;
+    cout << "Enter your choice: 0 to push, 1 to pop, 2 to print stack, 3 to exit: ";
+    cin >> choice;
 
-    cout<<"Enter your choice  0 to push , 1 to pop, 2 to print stack 3 to exit";
-    cin>>choice;
-
-    
-    while (choice!=3)
-    {
-        if(choice == 0){
-            push_stk(*ptr);
-
+    while (choice != 3) {
+        if (choice == 0) {
+            push_stk(&top);  // Push a new element onto the stack
+        } else if (choice == 1) {
+            pop_stk(&top);  // Pop the top element from the stack
+        } else if (choice == 2) {
+            prntstk(top);  // Print the stack
+        } else {
+            cout << "Invalid choice!" << endl;
         }
 
-        else if(choice == 1){
-            // pop_stk(*ptr);
-        }
-
-        else if(choice == 2){
-            // print_stk(ptr);
-
-        }
-    cout<<"Enter your choice  0 to push , 1 to pop, 2 to print stack, 3 to exit";
-    cin>>choice;
-        }
-        
-    
-
-
-
+        cout << "Enter your choice: 0 to push, 1 to pop, 2 to print stack, 3 to exit: ";
+        cin >> choice;
+    }
 
     return 0;
 }
